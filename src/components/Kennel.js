7555f6a58f254b148e36335a0b2562050ed476e6
Ashwin Prakash <ashp94@gmail.com>
2019-01-18 11:20:03 -0600

    Exercise for chapter 2 navbar owners-list

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,52 +1,18 @@
 import React, { Component } from "react"
-import EmployeeList from "./Employee/EmployeeList"
-import LocationList from "./Location/LocationList"
-import AnimalList from "./Animal/AnimalList"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+
 import "./Kennel.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 class Kennel extends Component {
-
-    /*
-        Although you will eventually be pulling your objects
-        from your json-server API, for this chapter, we're
-        faking it and just creating those arrays in the component
-        itself
-    */
-    // This will eventually get pulled from the API
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        {id:1, name: "dog"},
-        {id:2, name: "cat"},
-        {id:3, name: "lizard"},
-        {id:4, name: "tiger"}
-    ]
-
-
-    state = {
-        locations: this.locationsFromAPI,
-        employees: this.employeesFromAPI,
-        animals: this.animalsFromAPI
-    }
-
     render() {
         return (
-            <article className="kennel">
-                <LocationList locations={this.state.locations} />
-                <EmployeeList employees = {this.state.employees} />
-                <AnimalList animals = {this.state.animals}/>
-            </article>
+            <React.Fragment>
+                <NavBar />
+                <ApplicationViews />
+            </React.Fragment>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom"
 import React from 'react'
 import Kennel from "./components/Kennel"
+import {BrowserRouter as Router} from "react-router-dom"
 
-ReactDOM.render(<Kennel />, document.querySelector("#root"));
+ReactDOM.render(<Router><Kennel /></Router>,document.querySelector("#root"));
 
