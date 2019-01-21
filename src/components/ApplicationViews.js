6d989d768b828a06c657ec9e2b11352a65035c8d
Ashwin Prakash <ashp94@gmail.com>
2019-01-21 15:30:02 -0600

    started chp6

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,8 @@ import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnersList from './owners/OwnersList'
+import AnimalManager from "../modules/AnimalManager"
+
 
 
 export default class ApplicationViews extends Component {
@@ -14,35 +16,17 @@ export default class ApplicationViews extends Component {
         owners: []
     }
 
-    componentDidMount() {
-        const newState = {}
-
-        fetch("http://localhost:5002/animals")
-            .then(r => r.json())
-            .then(animals => newState.animals = animals)
-            .then(() => fetch("http://localhost:5002/employees")
-            .then(r => r.json()))
-            .then(employees => newState.employees = employees)
-            .then(()=> fetch("http://localhost:5002/owners")
-            .then(r => r.json()))
-            .then(owners => newState.owners = owners)
-            .then(()=> fetch("http://localhost:5002/locations")
-            .then(r => r.json()))
-            .then(locations => newState.locations = locations)
-            .then(()=> this.setState(newState))
-            //.then(animalData => this.setState({animals: animalData})
-        }
-        deleteAnimal = id => {
-            return fetch(`http://localhost:5002/animals/${id}`, {
-                method: "DELETE"
+    deleteAnimal = id => {
+        return fetch(`http://localhost:5002/animals/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/animals`))
+        .then(e => e.json())
+        .then(animals => this.setState({
+            animals: animals
             })
-            .then(e => e.json())
-            .then(() => fetch(`http://localhost:5002/animals`))
-            .then(e => e.json())
-            .then(animals => this.setState({
-                animals: animals
-            })
-          )
+            )
         }
 
         deleteEmployee = id => {
@@ -55,12 +39,12 @@ export default class ApplicationViews extends Component {
             .then(employees => this.setState({
                 employees: employees
             })
-          )
+            )
         }
 
         deleteOwner = id => {
             return fetch(`http://localhost:5002/owners/${id}`, {
-            method: "DELETE"
+                method: "DELETE"
             })
             .then(e => e.json())
             .then(() => fetch(`http://localhost:5002/owners`))
@@ -68,8 +52,28 @@ export default class ApplicationViews extends Component {
             .then(owners => this.setState({
                 owners: owners
             })
-          )
+            )
         }
+
+        componentDidMount() {
+            const newState = {}
+
+            fetch("http://localhost:5002/employees")
+                .then(r => r.json())
+                .then(employees => newState.employees = employees)
+                .then(()=> fetch("http://localhost:5002/owners")
+                .then(r => r.json()))
+                .then(owners => newState.owners = owners)
+                .then(()=> fetch("http://localhost:5002/locations")
+                .then(r => r.json()))
+                .then(locations => newState.locations = locations)
+                .then(()=> this.setState(newState))
+                AnimalManager.getAll().then(allAnimals => {
+                    this.setState({
+                        animals: allAnimals
+                    })
+                })
+            }
 
 
 
